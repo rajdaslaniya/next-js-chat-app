772dd0d49651ecb12f441314773a2644b506f395
jeet1desai <jeetdesai995@gmail.com>
2024-12-25 12:31:21 +0530

    fix: theme button color

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -12,7 +12,7 @@ export default function ThemeSwitcher() {
     <div className="fixed bottom-0 right-0 p-2 text-white rounded-md">
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
-          <Button variant="outline" size="icon">
+          <Button variant="outline" size="icon" className="text-secondary-foreground">
             <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
             <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           </Button>
